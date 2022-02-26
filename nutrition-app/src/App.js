00314eea7221a633fb6c33143136ecabe4c11072
Diego <74748954+futrhndrxx@users.noreply.github.com>
2022-02-26 00:23:57 -0800

    fetch()
    **not working atm***

--- nutrition-app/src/App.js
+++ nutrition-app/src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import FoodForm from './FoodForm'
 
 function App() {
+  
+  useEffect(() => {
+  fetch('/search/')
+  .then(response => {
+      console.log(response)
+     
+      response.json()
+      .then(data => {
+        console.log(data)
+      })
+    })
+  })
   return (
     <div className="App">
       <FoodForm></FoodForm>
