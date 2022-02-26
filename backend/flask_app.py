376d5af1dc330d483be9bed4c9738234be17bd63
“Jayden” <nhukhang1102@gmail.com>
2022-02-25 23:18:48 -0800

    server call api return response original json from api

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,12 +1,33 @@
-from crypt import methods
 from flask import Flask
 from flask import request
+import requests
+import json
+
+url = 'https://api.edamam.com/api/recipes/v2' 
+appid = "13d3ef3c"
+appkey = "c877e6b981dfe35b55518b76f6af9001"
+
+query = {
+    'type' :'public'
+}
+#?type=public&q=chicken&app_id=13d3ef3c&app_key=c877e6b981dfe35b55518b76f6af9001'
+
 
 app = Flask(__name__)
 
-@app.route("/search", methods = ["GET"])
+@app.route("/search/", methods = ["GET"])
 def searchRequest():
-    return "<p>Hello, World123!</p>"
+    # q = request.args.get('q')
+    q = "chicken"
+    query['q'] = q
+    query['app_id'] = appid
+    query['app_key'] = appkey
+    query['health'] = ['vegan', 'vegetarian']
+    response = requests.get(url, params = query)
+    # relist = {i['recipe']['label'] : 1 for i in response.json()['hits']}
+    # print(relist)
+    
+    return response.json()
 
 
 
