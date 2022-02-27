0cadc6b2ddc0f3c752f100ff0f5af820dc7d23bb
Diego Martinez <diegoam6@uci.edu>
2022-02-26 16:20:05 -0800

    Updated FoodForm, is now able to pass User Input to App.js

--- nutrition-app/src/FoodForm.js
+++ nutrition-app/src/FoodForm.js
@@ -1,34 +1,24 @@
 import React from "react";
 
-class FoodForm extends React.Component {
-    constructor(props) {
-      super(props);
-      // initialize an empty state
-      this.state = {value: ''};
-    
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+function FoodForm({ addUserInput }) {
+
+  const [userInput, setUserInput] = useState();
   
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('Entered Item: ' + this.state.value);
-      event.preventDefault();
-    }
-    
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <input type="text" placeholder="food or recipe" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+
+  const handleSubmit = (e) => {
+    addUserInput(userInput);
+    e.preventDefault();
   }
+ 
+  return (
+    <form onSubmit={e => {handleSubmit(e)}}>
+      
+      <input name="userInput" type="text" placeholder="food or recipe" value={userInput || ""} onChange={e => setUserInput(e.target.value)}/>      
+      <input className='submitButton' type="submit" value='Search' />
+
+     
+    </form>
+  )
+}
 
   export default FoodForm
